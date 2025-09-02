261255af3c81c6d3c68dd26fc90e235b73f608f1
GojoV339 <palle.venkat2005@gmail.com>
2025-09-02 10:33:26 +0530

    app.py file created

---
+++ app.py
@@ -0,0 +1,24 @@
+import pickle
+from flask import Flask,request,render_template
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+
+
+application = Flask(__name__) # entry point for the application
+
+app = application
+
+## Route
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/predictdata',methods = ['GET','POST'])
+def predict_datapoint():
+    if request.method == 'GET':
+        return render_template('home.html')
+    else:
+        data = CustomData
